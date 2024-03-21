67580829878641371cc633b2191ef0d7756954ae
Bill <bill.kimtai@student.moringaschool.com>
2024-03-21 09:56:52 +0300

    My Potforlio

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the button element
+  const button = document.getElementById("myButton");
+
+  // Add event listener to the button
+  button.addEventListener("click", function() {
+    // Perform some action when the button is clicked
+    console.log("Button clicked!");
+    // You can add any action you want here
+  });
+});
